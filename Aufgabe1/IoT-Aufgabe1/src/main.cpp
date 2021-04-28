b480c5e3998061dd3df3a3a897e0ba458b30d581
Niklas <niklas.bleicher@hs-furtwangen.de>
2021-04-28 21:05:31 +0200

    Created QR Code

--- Aufgabe1/IoT-Aufgabe1/src/main.cpp
+++ Aufgabe1/IoT-Aufgabe1/src/main.cpp
@@ -3,7 +3,9 @@
 #include <WebServer.h>
 #include "webapp.h"
 
-const int LED = 21;
+const int LEDEasy = 21;
+const int LEDMedium = 0;
+const int LEDHard = 0;
 const int touch0 = 4;
 const int touch1 = 0;
 const int touch2 = 2;
@@ -12,8 +14,6 @@ const char* ssid = "Test_Access-Point";
 const char* password = "test";
 
 WebServer server(80);
-
-String header;
 
 void handleRoot(){
     String html = webappIndex;
@@ -24,18 +24,16 @@ void setup() {
   Serial.begin(115200);
 
   //pin Modes for Buttons
-  pinMode(LED, OUTPUT);
-  pinMode(touch0, INPUT);
-  pinMode(touch1, INPUT);
-  pinMode(touch2, INPUT);
+  pinMode(LEDEasy, OUTPUT);
+  pinMode(touch0, INPUT); //Button Easy
+  pinMode(touch1, INPUT); //Button Medium
+  pinMode(touch2, INPUT); //Button Hard
 
-  //Access-Point
+  //Access-Point Settings
   WiFi.softAP(ssid, password);
   String html = webappIndex;
   server.on("/", handleRoot);
 
-  
-  
   //Get IP
   IPAddress IP = WiFi.softAPIP();
   Serial.println("IP-Adress");
@@ -46,17 +44,21 @@ void setup() {
 void loop() {
   //Access-Point
   server.handleClient();
+
+  //Change Hosted Page when Button pressed
+
     
-  //Touch Sensor Test
+  /*Touch Sensor Test
   Serial.println(digitalRead(touch0));
   if(digitalRead(touch0) == 1){
     Serial.println("Worked");
   }
-  delay(1000);
+  delay(1000);*/
 
   /*LED-Blink
   digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
   delay(1000);*/
+  
   }
