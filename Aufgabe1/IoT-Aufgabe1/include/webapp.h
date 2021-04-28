a14cf8daaad2b7df0494a1209ebbbba281da2e88
Niklas <niklas.bleicher@hs-furtwangen.de>
2021-04-28 19:54:55 +0200

    Added external Webpage

---
+++ Aufgabe1/IoT-Aufgabe1/include/webapp.h
@@ -0,0 +1,10 @@
+const char webappIndex[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<p>Hello World Test</p>
+</body>
+</html>
+)=====";
--- Aufgabe1/IoT-Aufgabe1/src/main.cpp
+++ Aufgabe1/IoT-Aufgabe1/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
+#include "webapp.h"
 
 const int LED = 21;
 const int touch0 = 4;
@@ -9,17 +11,32 @@ const int touch2 = 2;
 const char* ssid = "Test_Access-Point";
 const char* password = "test";
 
-WiFiServer server(80);
+WebServer server(80);
 
 String header;
 
+void handleRoot(){
+    String html = webappIndex;
+    server.send(200, "text/html", html);
+}
+
 void setup() {
   Serial.begin(115200);
+
+  //pin Modes for Buttons
   pinMode(LED, OUTPUT);
   pinMode(touch0, INPUT);
+  pinMode(touch1, INPUT);
+  pinMode(touch2, INPUT);
 
   //Access-Point
   WiFi.softAP(ssid, password);
+  String html = webappIndex;
+  server.on("/", handleRoot);
+
+  
+  
+  //Get IP
   IPAddress IP = WiFi.softAPIP();
   Serial.println("IP-Adress");
   Serial.println(IP);
@@ -28,24 +45,18 @@ void setup() {
 
 void loop() {
   //Access-Point
-  WiFiClient client = server.available();
-  if(client){
-    Serial.println("New User!");
-
-  if(client.connected()){
-      client.println("<!DOCTYPE html><html><head><title>Testpage</title></head><body><p>Hello World!</p></body></html>");
-  }
+  server.handleClient();
     
-  }
   //Touch Sensor Test
   Serial.println(digitalRead(touch0));
   if(digitalRead(touch0) == 1){
     Serial.println("Worked");
   }
   delay(1000);
-  //LED-Blink
+
+  /*LED-Blink
   digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
-  delay(1000);
+  delay(1000);*/
   }
